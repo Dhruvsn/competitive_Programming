41223f058f96a9095b1b9ece9949a3dc53ab56ac
Dhruv Sundan <2022a1r118@mietjammu.in>
2026-06-27 21:03:26 +0530

    744. Find Smallest Letter Greater Than Target

---
+++ patterns-wise-practice/arrays/binary_search/Easy/744_find_smallest_letter_greater_then_target.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int low = 0 , high = letters.size()-1;
+
+        while(low <=high){
+            int mid = low + (high - low)/2;
+
+            if(letters[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        return (low == letters.size()) ? letters[0] : letters[low];
+    }
+};
