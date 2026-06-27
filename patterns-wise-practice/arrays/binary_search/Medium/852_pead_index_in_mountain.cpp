af7daeb3d68b94e53b11a4a2ec2d8463e6813a7d
Dhruv Sundan <2022a1r118@mietjammu.in>
2026-06-27 21:17:54 +0530

    852 Peak Index in a Moutain Array

---
+++ patterns-wise-practice/arrays/binary_search/Medium/852_pead_index_in_mountain.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+public:
+  int peakIndexMountainArray(vector<int> & arr){
+      
+      int low = 0;
+      int high = arr.size()-1;
+
+      while(low < high){
+          int mid = low + (high - low)/2;
+
+          if(arr[mid] < arr[mid+1]){
+              low = mid + 1;
+          }
+          else{
+              high = mid;
+          }
+      }
+      
+      return low;
+  }
+};
