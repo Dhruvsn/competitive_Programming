6cd09df091916904563c1eb123cf446bf4ee743b
Dhruv Sundan <2022a1r118@mietjammu.in>
2026-06-30 13:26:41 +0530

    correct 33 Search In Roated Array

--- patterns-wise-practice/arrays/binary_search/Medium/33_Search_in_roated_arr.cpp
+++ patterns-wise-practice/arrays/binary_search/Medium/33_Search_in_roated_arr.cpp
@@ -2,24 +2,37 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-class Solution{
+class Solution {
 public:
-  int search(vector<int>& nums , int target){
-    int low = 0 ,high = nums.size()-1;
+    int search(vector<int>& nums, int target) {
+        
+        int s = 0,e = nums.size()-1;
+        
+        
 
-    while(low < high){
-      int mid = low + (high - low)/2;
+        while(s <= e){
+            int mid = s + (e - s)/2;
 
-      if(nums[mid] == target) return mid;
-      else if(nums[mid] > target) low = mid;
-      else high = mid - 1;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[s]<=nums[mid]){
+                if(target >=nums[s] && target < nums[mid]){
+                    e = mid-1;
+                }
+                else s = mid + 1;
+            }else{
+                if(target <= nums[e] && target > nums[mid]){
+                    s = mid + 1;
+                }else{
+                    e = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
-
-    return -1;
-  }
 };
-
   int main(){
     vector<int> arr = {1,3};
     Solution obj;
